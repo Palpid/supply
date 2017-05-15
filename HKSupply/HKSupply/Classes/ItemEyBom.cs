be5eb3fc36b8f6dbf36b0325591c091298b99d2f
mario.ruz <mario.ruz@etniabarcelona.com>
2017-05-15 16:00:51 +0200

    Pruebas pantalla BOM

---
+++ HKSupply/HKSupply/Classes/ItemEyBom.cs
@@ -0,0 +1,16 @@
+﻿using HKSupply.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKSupply.Classes
+{
+    public class ItemEyBom 
+    {
+        public ItemEy ItemEy { get; set; }
+        public List<Bom> RawMaterials { get; set; }
+        public List<Bom> Hardware { get; set; }
+    }
+}
